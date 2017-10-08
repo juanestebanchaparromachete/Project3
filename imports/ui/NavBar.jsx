5174304149b5829d89e069ebe22eb3465b6eb534
lfplazas10 <lf.plazas10@uniandes.edu.co>
2017-10-08 18:34:07 -0500

    Last styling changes

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -3,45 +3,45 @@ import {Link} from 'react-router-dom';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 const NavBar = () => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNavBar">
-        <div className="container">
-            <Link className="navbar-brand" id="oi" to='/projects'>Open Innovation</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <div className="text-grey">
-                    <AccountsUIWrapper id="hol"/>
-                </div>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/projects'>Proyectos</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/ideas'>Oportunidades</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/projects/create'>Crear Proyecto</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/ideas/create'>Crear oportunidad</Link>
-                    </li>
-                    {/*<li className="nav-item" style={{textAlign:'left'}}>*/}
-                    {/*<div id="loginWrapper" className="pull-left">*/}
-                    {/*/!*<AccountsUIWrapper className="dropdown-menu-left !important" style={{position: 'static'}}/>*!/*/}
-                    {/*</div>*/}
-                    {/*</li>*/}
-                </ul>
-            </div>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNavBar">
+    <div className="container">
+      <Link className="navbar-brand" id="oi" to='/projects'>Open Innovation</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+              aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <div className="text-grey navBartext" style={{marginTop: '5px'}}>
+          <AccountsUIWrapper id="hol"/>
         </div>
-    </nav>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link navBartext" to='/projects'>Proyectos</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link navBartext" to='/ideas'>Oportunidades</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link navBartext" to='/projects/create'>Crear Proyecto</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link navBartext" to='/ideas/create'>Crear oportunidad</Link>
+          </li>
+          {/*<li className="nav-item" style={{textAlign:'left'}}>*/}
+          {/*<div id="loginWrapper" className="pull-left">*/}
+          {/*/!*<AccountsUIWrapper className="dropdown-menu-left !important" style={{position: 'static'}}/>*!/*/}
+          {/*</div>*/}
+          {/*</li>*/}
+        </ul>
+      </div>
+    </div>
+  </nav>
 );
 
 $(document).on('scroll', function (e) {
-    let op = 1 - ($(document).scrollTop() / 5000);
-    if (op > 0.65)
-        $('#mainNavBar').css('opacity', op);
+  let op = 1 - ($(document).scrollTop() / 5000);
+  if (op > 0.65)
+    $('#mainNavBar').css('opacity', op);
 });
 
 export default NavBar;
