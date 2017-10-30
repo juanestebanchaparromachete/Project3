9dd9e7383bcbc01b76c6072ffbe392bb4d501fd1
lfplazas10 <lf.plazas10@uniandes.edu.co>
2017-10-30 16:19:02 -0500

    Added more task tests

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -8,9 +8,8 @@ import faker from "faker";
 if (Meteor.isServer) {
   describe('tasks', function () {
 
-    describe('tasks.insert', function () {
-
-      let tempObject = {
+    function createTestTask() {
+      let obj = {
         count: faker.Random,
         name: faker.name.findName(),
         slogan: faker.name.findName(),
@@ -19,9 +18,12 @@ if (Meteor.isServer) {
         requirements: [faker.lorem.sentence(), faker.lorem.sentence(), faker.lorem.words()],
         stage: faker.lorem.word()
       };
+      return obj;
+    }
+
+    describe('tasks.insert', function () {
 
       beforeEach(function () {
-        // Tasks.insert(tempObject);
         resetDatabase();
         Factory.define('user', Meteor.users, {
           username: faker.name.findName(),
@@ -36,7 +38,8 @@ if (Meteor.isServer) {
         resetDatabase();
       });
 
-      it('should add a new task/project', function () {
+      it('Should add a new task/project', function () {
+        let tempObject = createTestTask();
         Meteor.call('tasks.insert', tempObject)
 
         let retreivedTask = Tasks.find({name:tempObject.name}).fetch()[0];
@@ -46,6 +49,39 @@ if (Meteor.isServer) {
         assert.equal(tempObject.name, retreivedTask.name)
         assert.equal(tempObject.slogan, retreivedTask.slogan)
         assert.equal(tempObject.thumbnail, retreivedTask.thumbnail)
+        assert.deepEqual(tempObject.requirements, retreivedTask.requirements)
+        assert.equal(tempObject.stage, retreivedTask.stage)
+      })
+    })
+
+    describe('tasks.remove', function () {
+
+      let tempId;
+
+      beforeEach(function () {
+        resetDatabase();
+        Factory.define('user', Meteor.users, {
+          username: faker.name.findName(),
+        });
+        currentUser = Factory.create('user');
+        sinon.stub(Meteor, 'user');
+        Meteor.user.returns(currentUser);
+
+        tempId = Tasks.insert(createTestTask());
+      })
+
+      afterEach(() => {
+        Meteor.user.restore();
+        resetDatabase();
+      });
+
+      it('Should delete the previously inserted task/project', function () {
+        Meteor.call('tasks.remove', tempId)
+
+        let retreivedTask = Tasks.find({_id:tempId}).fetch()[0];
+        console.log(retreivedTask)
+
+        assert.isUndefined(retreivedTask);
       })
     })
   })
